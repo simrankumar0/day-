2c1e4df224fd13331b562a5008f74b46ca346136
simrankumar0 <simrankumar2010@icloud.com>
2025-06-29 16:59:01 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,7 @@
+operator = input("What operator do you want to use?")
+num1 = input("what is your first number?")
+num2 = input("what is your second number?")
+if operator == "add":
+    print(num1 +num2)
+elif operator == "subtract":
+    print(num1 - num2)
