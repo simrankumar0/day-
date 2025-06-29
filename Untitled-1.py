c5fe6e02d3e9ba67c149530f8377e6f74a3e3a4b
simrankumar0 <simrankumar2010@icloud.com>
2025-06-29 16:56:37 -0400

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,29 @@
+var= "Simran"
+print("Hi my name is " + var)
+print("Hi my name is", var)
+
+# adj1= "yellow" 
+# verb1 = "ran"
+# noun1 = "park"
+# noun2 = "monkey"
+# verb2 = "singing"
+# verb3 = "eating"
+# noun3 = "apple"
+
+
+adj1= input("give me an adjective")
+verb1 = input("give me a verb")
+noun1 = input("give me a noun")
+noun2 = input("give me a noun")
+verb2 = input("give me a verb")
+verb3 = input("give me a verb")
+noun3 = input("give me a noun")
+
+
+print("Yesterday I", verb1, "to the", noun1, "and I saw a giant", adj1, noun2, "the", noun2, "was", verb2, "very loudly and", verb3, "an", noun3)
+
+x =4
+y=8
+print(x+y)
+
+
